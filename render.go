bcd4094f0918862f471d3c64c372e1cf2a099826
Matt Baer <dev@baer.works>
2020-02-29 11:17:48 -0500

    Log parse and render errors

--- render.go
+++ render.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"io"
 	"io/ioutil"
+	"log"
 )
 
 func Render(m *Mixtape, w io.Writer) error {
@@ -15,15 +16,22 @@ func Render(m *Mixtape, w io.Writer) error {
 	}
 	mixtapeRawTmpl, err := ioutil.ReadFile("mixtape.tmpl")
 	if err != nil {
+		log.Print("Unable to load local mixtape.tmpl; falling back to default")
 		mixtapeRawTmpl, err = ReadAsset("mixtape.tmpl", false)
 		if err != nil {
 			return err
 		}
+	} else {
+		log.Print("Generating from local mixtape.tmpl")
 	}
 	t, err := template.New("mixtape").Parse(string(mixtapeRawTmpl) + string(partsRawTmpl))
 	if err != nil {
+		log.Printf("[ERROR] Unable to parse: %v", err)
 		return err
 	}
-	t.ExecuteTemplate(w, "mixtape", m)
+	err = t.ExecuteTemplate(w, "mixtape", m)
+	if err != nil {
+		log.Printf("[ERROR] Unable to render: %v", err)
+	}
 	return nil
 }
