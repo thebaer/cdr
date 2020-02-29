c71c124bcbf88a81c14fde2a044ac3fd5431f387
Matt Baer <dev@baer.works>
2020-02-29 11:42:00 -0500

    Fix site on mobile
    plus set charset = UTF-8

--- templates.go
+++ templates.go
@@ -22,6 +22,8 @@ var files = map[string]string{
 <html>
 	<head>
 		<title>Mixtape</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<style type="text/css">
 			body {
 				font-size: 1.2em;
